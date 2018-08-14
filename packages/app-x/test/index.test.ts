d93b6cac6080f965d9fc52595d83f753ab34fbd7
d10221 <danieliozzi@gmail.com>
2018-08-14 06:10:57 +0200

    test-subject : expectations

--- packages/app-x/test/index.test.ts
+++ packages/app-x/test/index.test.ts
@@ -2,6 +2,6 @@ import { join } from "path";
 import { debug } from "../";
 describe(require(join(__dirname, "../package.json")).name, () => {
   it("works", () => {
-    expect(debug.namespace).toBe("local/module-x");
+    expect(debug.namespace).toBe("local/app-x");
   });
 });
